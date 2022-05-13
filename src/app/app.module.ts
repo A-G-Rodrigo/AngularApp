449d96500c8cc6c42e87f593d9e0ebe2ae103645
agrssanchez@outlook.es <agrssanchez@outlook.es>
2022-05-13 12:41:16 +0200

    version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SectionComponent } from './section/section.component';
+import { HomeAngularComponent } from './home-angular/home-angular.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SectionComponent,
+    HomeAngularComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home-angular/home-angular.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-angular',
+  templateUrl: './home-angular.component.html',
+  styleUrls: ['./home-angular.component.css']
+})
+export class HomeAngularComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
